cb546dbbcb474ab783bd588dbb9d5741198f94b6
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2025-07-27 13:56:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (19.27%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
-    // Brute Force approach -> O(n2)
-    ListNode* getNodeAt(ListNode* head, int idx){
-        ListNode* temp = head;
-        for(int i=1;i<=idx;i++){
-            temp=temp->next; 
-        }
-        return temp;
-    }
     ListNode* reverseList(ListNode* head) {
-        int n = 0;
-        ListNode*temp = head;
-        // Calculate the length of the linked list
-        while(temp!=NULL){
-            temp = temp->next;
-            n++;    // length of the linkedlist is calculated
+        // Iterative way
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* Next = head; //Remember that "next" is not same as Next;
+        while(curr!=NULL){
+            Next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = Next;
         }
-        int i= 0;
-        int j = n-1;
-        while(i<j){
-            ListNode* left = getNodeAt(head, i);
-            ListNode* right = getNodeAt(head, j);
-            int t = left->val;
-            left->val = right->val;
-            right->val = t;
-            i++;
-            j--;
-        }
-        return head;
+        return prev;
     }
 };
