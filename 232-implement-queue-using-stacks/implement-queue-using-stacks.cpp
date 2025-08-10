d4d08ed425ca3bf6dd2b7e5fff2ea3765dd2c9c9
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2025-08-10 16:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (26.72%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -7,35 +7,32 @@ public:
     }
     
     void push(int x) {
-        st.push(x);
+        // Push at Bottom
+        if(st.size()==0){
+            st.push(x);
+            return ;
+        }
+        else{
+            while(st.size()!=0){
+                helper.push(st.top()); 
+                st.pop();
+            }
+            st.push(x);
+            while(helper.size()!=0){
+                st.push(helper.top());
+                helper.pop();
+            } 
+        }
     }
     
-    int pop() { // Remove at bottom || T.C = O(n)
-        while(st.size()!=0){
-            helper.push(st.top()); 
-            st.pop();
-        }
-        int x = helper.top();
-        helper.pop();
-        while(helper.size()!=0){
-            st.push(helper.top());
-            helper.pop();
-        }
+    int pop() { // T.C = O(1)
+        int x = st.top();
+        st.pop();
         return x;
     }
     
-    int peek() { // front || T.C = O(n)
-        // Retrieve at bottom
-        while(st.size()!=0){
-            helper.push(st.top()); 
-            st.pop();
-        }
-        int x = helper.top();
-        while(helper.size()!=0){
-            st.push(helper.top());
-            helper.pop();
-        }
-        return x;
+    int peek() { // front || T.C = O(1)
+        return st.top(); 
     }
     
     bool empty() {
