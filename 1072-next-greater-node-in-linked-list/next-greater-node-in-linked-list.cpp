81d9430bab8e399611ee0cbdac3af9748ebe7be3
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2025-08-13 19:53:05 +0530

    Time: 1 ms (89.30%) | Memory: 47.2 MB (30.38%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
-        stack<ListNode*> st;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            while(st.size()>0 && st.top()->val < temp->val){
-                ListNode* helper = st.top();
-                st.pop();
-                helper->val = temp->val;
-            }
-            st.push(temp);
-            temp = temp->next;
-        }
-        while(st.size()>0){
-            ListNode* helper = st.top();
-            st.pop();
-            helper->val = 0;
-        }
-        vector<int> ans;
+        vector<int> values;
         while(head!=NULL){
-            ans.push_back(head->val);
+            values.push_back(head->val);
             head = head->next;
+        }
+        int n = values.size();
+        vector<int> ans(n,0);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(st.size()>0 && values[st.top()]<values[i]){
+                ans[st.top()] = values[i];
+                st.pop();
+            }
+            st.push(i);
         }
         return ans;
     }
