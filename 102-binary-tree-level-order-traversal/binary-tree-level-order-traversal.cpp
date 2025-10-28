78b57be29b775c2680ae11ad9a43504a273f3427
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2025-10-28 10:35:54 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (99.61%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -4,26 +4,20 @@ public:
         if(root==NULL) return 0;
         return 1 + max(levels(root->left), levels(root->right));
     }
-    void nthLevel(TreeNode* root, int curr, int level, vector<int>& v){
+    void lorder(TreeNode* root, vector<vector<int>>& ans, int level){
         if(root==NULL) return;
-        if(curr==level){
-            v.push_back(root->val);
-            return;
-        }
-        nthLevel(root->left, curr+1, level, v);
-        nthLevel(root->right, curr+1, level, v);
-    }
-    void lorder(TreeNode* root, vector<vector<int>>& ans){
-        int n = levels(root);
-        for(int i=1;i<=n;i++){
-            vector<int> v;
-            nthLevel(root,1,i,v);
-            ans.push_back(v);
-        }
+        ans[level].push_back(root->val);
+        lorder(root->left,ans,level+1);
+        lorder(root->right,ans,level+1);
     }
     vector<vector<int>> levelOrder(TreeNode* root) {
+        int n = levels(root);
         vector<vector<int>> ans;
-        lorder(root,ans);
+        for(int i=1;i<=n;i++){
+            vector<int> v;
+            ans.push_back(v);
+        }
+        lorder(root,ans,0);
         return ans;
     }
 };
