38f2eba208391b333c4da976c8717a7d7300167f
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2025-10-28 16:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (60.89%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -4,24 +4,15 @@ public:
         if(root==NULL) return 0;
         return 1 + max(levels(root->left),levels(root->right));
     }
-    void nthLevel(TreeNode* root, int curr, int level, vector<int>& ans){
+    void preOrder(TreeNode* root, vector<int>& ans, int level){
         if(root==NULL) return;
-        if(curr==level){
-            ans[curr] = root->val;
-            return;
-        }
-        nthLevel(root->left,curr+1,level,ans);
-        nthLevel(root->right,curr+1,level,ans);
-    }
-    void levelOrder(TreeNode* root, vector<int>& ans){
-        int n = ans.size();
-        for(int i=0;i<n;i++){
-            nthLevel(root,0,i,ans);
-        }
+        ans[level] = root->val;
+        preOrder(root->left,ans,level+1);
+        preOrder(root->right,ans,level+1);
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans(levels(root),0);
-        levelOrder(root,ans);
+        preOrder(root,ans,0);
         return ans;
     }
 };
