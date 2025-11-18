17f0ae30c089f053c8393366e0556f21b2140fc2
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2025-11-18 16:36:32 +0530

    Time: 31 ms (15.64%) | Memory: 38.6 MB (28.86%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    typedef pair<int,int> pi;
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pi> pq; // Max Heap
+        for(int ele: arr){
+            int distance = abs(x-ele);
+            pi p = {distance,ele};
+            pq.push(p);
+            if(pq.size()>k) pq.pop();
+        }
+        vector<int> ans;
+        while(pq.size()>0){
+            int ele = pq.top().second;
+            ans.push_back(ele);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
