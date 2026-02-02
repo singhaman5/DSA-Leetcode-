cd9f89e4bf2fcc82fc4c03099cd60287904102c4
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2026-02-02 17:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.54%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int prev1 = 0;  // max money till previous house
-        int prev2 = 0;  // max money till the house before previous
-        for (int num : nums) {
-            int take = prev2 + num;   // rob this house
-            int skip = prev1;         // skip this house
-            int curr = max(take, skip);
-            prev2 = prev1;   // move forward
-            prev1 = curr;
+    vector<int> dp;
+    int f(vector<int> arr, int i){
+        if(i==arr.size()-1) return arr[i]; // single house
+        if(i==arr.size()-2) return max(arr[i],arr[i+1]); // 2 houses
+        return max(arr[i] + f(arr, i+2),0+f(arr,i+1));
+    }
+    int ftd(vector<int> arr, int i){
+        if(i==arr.size()-1) return arr[i]; // single house
+        if(i==arr.size()-2) return max(arr[i],arr[i+1]); // 2 houses
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = max(arr[i] + f(arr, i+2),0+ftd(arr,i+1));
+    }
+    int fbu(vector<int> arr){
+        int n = arr.size();
+        if(n==1) return arr[0];
+        dp.clear();
+        dp.resize(n);
+        //base case
+        dp[n-1] = arr[n-1];
+        dp[n-2] = max(arr[n-1],arr[n-2]);
+        for(int i=n-3;i>=0;i--){
+            dp[i] = max(arr[i] + dp[i+2], 0 + dp[i+1]);
         }
-        return prev1;
+        return dp[0];
+    }
+    int rob(vector<int>& arr) {
+        return fbu(arr);
     }
 };
