40e2e2a4ec7fee11cf5233ca96b52c3357549254
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2026-02-06 16:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (75.64%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMaxSum(vector<int>& arr) {
+        int n = arr.size();
+        int prev = arr[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int take = arr[i];
+            if(i>1) take += prev2;
+            int notTake = 0 + prev;
+            int curi = max(take,notTake);
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> temp1, temp2;
+        for(int i=0;i<n;i++){
+            if(i != 0) temp1.push_back(nums[i]);
+            if(i != n-1) temp2.push_back(nums[i]);
+        }
+        return max(findMaxSum(temp1),findMaxSum(temp2));
+    }
+}; 
