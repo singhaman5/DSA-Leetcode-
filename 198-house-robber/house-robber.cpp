819f153aa3fc591be67248ab36b3c011a5a40630
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2026-02-06 15:34:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.42%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    vector<int> dp;
-    int f(vector<int> arr, int i){
-        if(i==arr.size()-1) return arr[i]; // single house
-        if(i==arr.size()-2) return max(arr[i],arr[i+1]); // 2 houses
-        return max(arr[i] + f(arr, i+2),0+f(arr,i+1));
+    int f(int idx, vector<int> &nums, vector<int> &dp){
+        if(idx==0) return nums[0];
+        if(idx<0) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int pick = nums[idx] + f(idx-2,nums,dp);
+        int notPick = 0 + f(idx-1,nums,dp);
+        return dp[idx] = max(pick,notPick);
     }
-    int ftd(vector<int> arr, int i){
-        if(i==arr.size()-1) return arr[i]; // single house
-        if(i==arr.size()-2) return max(arr[i],arr[i+1]); // 2 houses
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = max(arr[i] + f(arr, i+2),0+ftd(arr,i+1));
-    }
-    int fbu(vector<int> arr){
-        int n = arr.size();
-        if(n==1) return arr[0];
-        dp.clear();
-        dp.resize(n);
-        //base case
-        dp[n-1] = arr[n-1];
-        dp[n-2] = max(arr[n-1],arr[n-2]);
-        for(int i=n-3;i>=0;i--){
-            dp[i] = max(arr[i] + dp[i+2], 0 + dp[i+1]);
-        }
-        return dp[0];
-    }
-    int rob(vector<int>& arr) {
-        return fbu(arr);
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        return f(n-1,nums,dp);
     }
 };
