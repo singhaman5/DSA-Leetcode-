3126cd010ad6f591c9fd1311e6a85ac0b6d8c287
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2026-03-18 12:58:18 +0530

    Time: 3 ms (91.84%) | Memory: 13.2 MB (59.04%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     // TABULATION CODE
-    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(i==0) return j;
-        if(j==0) return i;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i-1] == s2[j-1]) return dp[i][j] = f(i-1,j-1,s1,s2,dp);
-        return dp[i][j] =  1 + min(f(i-1,j,s1,s2,dp), min(f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)));
-    }
-    int minDistance(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
-        return f(n,m,str1,str2,dp);
+    int minDistance(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n+1, vector<int> (m+1,0));
+        for(int i=0; i<=n; i++) dp[i][0] = i;
+        for(int j=0; j<=m; j++) dp[0][j] = j;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] =  1 + min(dp[i-1][j], min(dp[i][j-1],dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
