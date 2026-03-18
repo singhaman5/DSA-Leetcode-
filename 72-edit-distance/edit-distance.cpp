68afbf43e466a0951346cbea4db29bcd95a40068
singhaman5 <152908579+singhaman5@users.noreply.github.com>
2026-03-18 12:48:19 +0530

    Time: 8 ms (46.77%) | Memory: 13.3 MB (30.41%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    // TABULATION CODE
     int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+        if(i==0) return j;
+        if(j==0) return i;
         if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i] == s2[j]) return dp[i][j] = f(i-1,j-1,s1,s2,dp);
+        if(s1[i-1] == s2[j-1]) return dp[i][j] = f(i-1,j-1,s1,s2,dp);
         return dp[i][j] =  1 + min(f(i-1,j,s1,s2,dp), min(f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)));
     }
     int minDistance(string str1, string str2) {
         int n = str1.size();
         int m = str2.size();
-        vector<vector<int>> dp(n, vector<int> (m,-1));
-        return f(n-1,m-1,str1,str2,dp);
+        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        return f(n,m,str1,str2,dp);
     }
 };
