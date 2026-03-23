b90140ecfdd5c43a86ac0d0b78b2e3e53c4ed204
Aman Singh <152908579+singhaman5@users.noreply.github.com>
2026-03-23 11:10:37 +0530

    Time: 6 ms (60.91%) | Memory: 14.4 MB (88.00%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    // MEMOIZATION SOLUTION -: 
-    int f(int ind, int tranNo, vector<int> &prices, int n, int k, vector<vector<int>> &dp){
-        if(ind == n || tranNo == 2*k) return 0;
-        if(dp[ind][tranNo] != -1) return dp[ind][tranNo];
-        if(tranNo % 2 == 0){
-            return dp[ind][tranNo] = max(-prices[ind] + f(ind+1, tranNo+1, prices, n, k,dp),
-            0 + f(ind+1, tranNo, prices, n, k,dp));
-        }
-        return dp[ind][tranNo] = max(prices[ind] + f(ind+1, tranNo+1, prices, n, k,dp),
-        0 + f(ind+1, tranNo, prices, n, k,dp));
-    }
+    // TABULATION with OPTIMISATION SOLUTION -: 
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2*k,-1));
-        return f(0,0,prices,n,k,dp);
+        vector<int> after(2*k+1,0);
+        vector<int> cur(2*k+1,0);
+        for(int ind= n-1;ind>=0;ind--){
+            for(int tranNo= 2*k-1; tranNo >= 0; tranNo--){
+                if(tranNo % 2 == 0){
+                    cur[tranNo] = max(-prices[ind] + after[tranNo+1],
+                    after[tranNo]);
+                }
+                else  cur[tranNo] = max(prices[ind] + after[tranNo+1],after[tranNo]);
+                }
+                after = cur;
+            }
+        return after[0];
     }
 };
