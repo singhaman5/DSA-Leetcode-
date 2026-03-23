059f0f4d0a4fdc732a621b4fefeecd036e22714b
Aman Singh <152908579+singhaman5@users.noreply.github.com>
2026-03-23 10:58:49 +0530

    Time: 11 ms (42.70%) | Memory: 17.8 MB (53.70%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size(); 
-        vector<vector<int>> after(2, vector<int>(k+1, 0));
-        vector<vector<int>> cur(2, vector<int>(k+1, 0));
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                for (int cap = 1; cap <= k; cap++) {
-                    if (buy == 0) { 
-                        cur[buy][cap] = max(
-                            after[0][cap], 
-                            -prices[ind] + after[1][cap]
-                        );
-                    }
-                    else { 
-                        cur[buy][cap] = max(
-                            after[1][cap],
-                            prices[ind] + after[0][cap - 1]
-                        );
-                    }
-                }
-            }
-            after = cur;
+    // MEMOIZATION SOLUTION -: 
+    int f(int ind, int tranNo, vector<int> &prices, int n, int k, vector<vector<int>> &dp){
+        if(ind == n || tranNo == 2*k) return 0;
+        if(dp[ind][tranNo] != -1) return dp[ind][tranNo];
+        if(tranNo % 2 == 0){
+            return dp[ind][tranNo] = max(-prices[ind] + f(ind+1, tranNo+1, prices, n, k,dp),
+            0 + f(ind+1, tranNo, prices, n, k,dp));
         }
-        return after[0][k]; 
+        return dp[ind][tranNo] = max(prices[ind] + f(ind+1, tranNo+1, prices, n, k,dp),
+        0 + f(ind+1, tranNo, prices, n, k,dp));
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2*k,-1));
+        return f(0,0,prices,n,k,dp);
     }
 };
