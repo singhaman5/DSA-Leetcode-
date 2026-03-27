14c65a48303eeb7c6af087a75b387d3310340f3c
Aman Singh <152908579+singhaman5@users.noreply.github.com>
2026-03-27 16:55:52 +0530

    Time: 1504 ms (36.70%) | Memory: 9.1 MB (94.66%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -8,23 +8,21 @@ public:
         }
         return true;
     }
-    int f(int i, int n, string &s, vector<int> &dp){
-        if(i==n) return 0;
-        if(dp[i] != -1) return dp[i];
-        string temp = " ";
-        int minCost = INT_MAX;
-        for(int j=i; j<n; j++){
-            temp += s[j];
-            if(isPalindrome(i,j,s)){
-            int cost = 1 + f(j+1,n,s,dp);
-            minCost = min(minCost, cost);
-            }
-        }
-        return dp[i] = minCost;
-    }
     int minCut(string s) {
+        // Tabulation Code -: 
         int n = s.size();
-        vector<int> dp(n,-1);
-        return f(0,n,s,dp) - 1;
+        vector<int> dp(n+1,0);
+        dp[n] = 0;
+        for(int i=n-1; i>=0; i--){
+            int minCost = INT_MAX;
+            for(int j=i; j<n; j++){
+                if(isPalindrome(i,j,s)){
+                int cost = 1 + dp[j+1];
+                minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] - 1;
     }
 };
