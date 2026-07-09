17e0bcb792aa66f4d078ff8410b17196ad2ec786
Aman Singh <152908579+singhaman5@users.noreply.github.com>
2026-07-09 10:48:12 +0530

    Time: 635 ms (28.55%) | Memory: 295.5 MB (37.41%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,36 @@
 class Solution {
 public:
+    void merge(vector<int>& arr, int l, int mid, int r){
+        vector<int> temp;
+        int i = l;
+        int j = mid + 1;
+        while(i <= mid && j <= r){
+            if(arr[i] <= arr[j]){
+                temp.push_back(arr[i++]);
+            }
+            else{
+                temp.push_back(arr[j++]);
+            }
+        }
+        while(i <= mid){
+            temp.push_back(arr[i++]);
+        }
+        while(j <= r){
+            temp.push_back(arr[j++]);
+        }
+        for(int i=l; i<=r; i++){
+            arr[i] = temp[i-l];
+        }
+    }
+    void mergeSort(vector<int>& arr, int l, int r){
+        if(l >= r) return;
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
 };
