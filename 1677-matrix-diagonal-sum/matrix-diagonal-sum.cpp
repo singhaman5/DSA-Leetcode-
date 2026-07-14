19be90f793e68a602697f6b0bb2d3ae06f80d984
Aman Singh <152908579+singhaman5@users.noreply.github.com>
2026-07-14 09:27:46 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (16.40%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -5,7 +5,7 @@ public:
         int sum=0;
         for(int i=0; i<n; i++) {
             sum += mat[i][i];              
-            sum += mat[i][n - i - 1];      
+            sum += mat[i][n-i-1];      
         }
         if(n%2==1) {
             sum -= mat[n/2][n/2];
